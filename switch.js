1a6ab8d75a73277bcff0d405f094017b684f47c3
Olga Smirnova <o.v.smirnova1987@gmail.com>
2017-04-17 21:34:32 +0300

    add script with my timetable for week

--- switch.js
+++ switch.js
@@ -1,3 +1,25 @@
 /**
  * Created by Olga on 4/17/17.
  */
+var day = prompt("Write day of the week!", " ");
+switch(day){
+    case 'Monday':
+        alert('Today I have English lesson');
+    case 'Tuesday':
+        alert('Today  I have training in gym');
+        break;
+    case 'Wednesday':
+        alert('Today, English lesson, again!)))');
+        break;
+    case 'Thursday':
+        alert('Today I have yoga training! Ooom!))');
+        break;
+    case 'Friday':
+        alert("Today, I'm stay at home and learn JS");
+        break;
+
+    case 'Saturday':
+    case 'Sunday':
+        alert('Yeah! It is weekend, and I will do nothing!');
+        break;
+}
